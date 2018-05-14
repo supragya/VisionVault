9321300a92f62f9abc735585de55cbf979643402
Supragya Raj <supragyaraj@gmail.com>
2018-05-14 13:39:47 +0530

    Initial structure

---
+++ src/stream_handler/controlman.cpp
@@ -0,0 +1,5 @@
+//
+// Created by supragya on 14/5/18.
+//
+
+#include "framemanager.h"
---
+++ src/stream_handler/metamanger.h
@@ -0,0 +1,8 @@
+//
+// Created by supragya on 14/5/18.
+//
+
+#ifndef FRAMEMANAGER_H
+#define FRAMEMANAGER_H
+
+#endif
