c1401412495420baa7bbd306d47df75ddcee760b
Supragya Raj <supragyaraj@gmail.com>
2018-05-17 09:35:42 +0530

    MLV header added, wrote simple generator (not compiled)
    Signed-off-by: Supragya Raj <supragyaraj@gmail.com>

--- src/generator/generator.cpp
+++ src/generator/generator.cpp
@@ -1,5 +1,76 @@
 //
-// Copyright 2018, Supragya Raj licensed under MIT License (for RawStreamHandler).
+// Copyright 2018, Supragya Raj
+// licensed under MIT License (for RawStreamHandler).
 //
 
-#include "framemanager.h"
+#include <iostream>
+#include <fstream>
+#include "generator.h"
+#include "../common/mlv.h"
+using namespace std;
+
+// Synthetic RAW12 generator
+// type = 1 (red), 2 (green), 3 (blue)
+uint8_t* GetRaw12Frame(int type){
+    uint8_t* rawFrame = new uint8_t[18*1024*1024];
+
+    // Using RGGB pattern
+    int curptr = 0;
+    for(int i = 0; i<3072/2; i++){
+        // All row pairs of sensel data
+        for(int j=0; j<2; j++){
+            if(j == 0){
+                // Odd row
+                for(int k=0; k<4096/2; k++){
+                    rawFrame[curptr++] = type==1 ? 255 : 0;
+                    rawFrame[curptr++] = type==1 ? 240 : type == 2? 15 : 0;
+                }
+            }
+            else{
+                // Even row
+                for(int k=0; k<4096/2; k++){
+                    rawFrame[curptr++] = type==2 ? 255 : 0;
+                    rawFrame[curptr++] = type==2 ? 240 : type == 3? 15 : 0;
+                }
+            }
+        }
+    }
+
+    return rawFrame;
+}
+
+int main(){
+    fileStream frameStream("FrameStream.dat", streamType.frame);
+
+    // Begin writing to FrameData
+
+    // MLVI block to begin the frame stream
+    mlv_file_hdr_t mlviHdr;
+    Populate(&mlviHdr);
+    frameStream.write2file(reinterpret_cast<void*>(mlviHdr), sizeof(mlviHdr));
+
+    // RAWI block to set raw mode
+    mlv_rawi_hdr_t rawiHdr;
+    Populate(&rawiHdr);
+    frameStream.write2file(reinterpret_cast<void*>(rawiHdr), sizeof(rawiHdr));
+
+    // Get a few Red/Green/Blue RAW12 frames (synthetic)
+    uint8_t *colorFrames[] = {GetRaw12Frame(1), GetRaw12Frame(2), GetRaw12Frame(3)};
+    mlv_vidf_hdr_t templateVidf;
+    Populate(&templateVidf);
+
+    // Write Frames 300 in number
+    for(int i=0; i<100; i++){
+        for(int j=0; j<3; j++){
+            templateVidf.timestamp = i*3+j;
+            frameStream.write2file(templateVidf, sizeof(templateVidf));
+            frameStream.write2file(colorFrames[j], 18*1024*1024);
+        }
+    }
+
+    // Begin writing metadata
+    fileStream metaStream("MetaStream.dat", streamType.meta);
+
+
+    return 0;
+}
--- src/generator/generator.h
+++ src/generator/generator.h
@@ -1,5 +1,41 @@
 //
-// Copyright 2018, Supragya Raj licensed under MIT License (for RawStreamHandler).
+// Copyright 2018, Supragya Raj
+// licensed under MIT License (for RawStreamHandler).
 //
 
-#include "framemanager.h"
+#ifndef GENERATOR_H
+#define GENERATOR_H
+
+#include <fstream>
+#include <string>
+using namespace std;
+
+enum streamType{
+    frame,
+    meta
+};
+
+class fileStream{
+private:
+    std::ofstream file;
+    streamType type;
+public:
+    fileStream(string fileName, bool sType){
+        file.open(fileName, ios::out| ios::trunc);
+        type = sType ? streamType.frame : streamType.meta;
+
+        cout<<"FileOpen: "<<fileName<<" "<< file.is_open() ? "Success ": "Failure ";
+        cout<<", StreamType: "<< sType ? "Frame " : "Meta "<<endl;
+    }
+    bool write2file(void* data, int len){
+        if(file.is_open()){
+            file.write(data, len);
+        }
+    }
+    ~fileStream(){
+        // Cleanup
+        file.close();
+    }
+};
+
+#endif
