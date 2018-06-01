80ba06f40a59912e8e7a0f88a36a5d4100b611f9
Supragya Raj <supragyaraj@gmail.com>
2018-06-01 10:25:34 +0530

    FrameStreamHandler wrote for vidf

--- src/stream_handler/frameman.cpp
+++ src/stream_handler/frameman.cpp
@@ -1,6 +1,10 @@
 #include "../common/mlv.h"
 #include "frameman.h"
 #include <thread>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <memory.h>
 
 using namespace RawStreamHandler;
 
@@ -22,9 +26,72 @@ void RawStreamHandler::FrameManEntry() {
 }
 
 void RawStreamHandler::FrameStreamHandler(bool *syncbool, FrameBuffer *buf, const char *streamloc) {
-    return;
+    int exit_reason = 0;
+    // Reasons: noFileStream(0), syncbool(1), endofstream(2)
+
+    std::ifstream fStream(streamloc);
+
+    if (!fStream.is_open()) {
+        *syncbool = false;
+    } else {
+        std::cout << "FrameStreamHandler success opening " << streamloc << std::endl;
+    }
+
+    if (*syncbool)
+        exit_reason = 1;
+
+    mlv_vidf_hdr_t chunk = {0};
+    bool entrypossible;
+    int i;
+    while (*syncbool) {
+        if (fStream.eof()) {
+            exit_reason = 2;
+            break;
+        }
+        fStream.read(reinterpret_cast<char *>(&chunk), sizeof(chunk));
+        entrypossible = false;
+        do {
+            for (i = 0; i < 2; i++) {
+                if (buf->filled[i])
+                    continue;
+                else if (buf->offset[i] + sizeof(chunk) > buf->bufsize) {
+                    buf->filled[i] = true;
+                    continue;
+                } else {
+                    entrypossible = true;
+                    break;
+                }
+            }
+        } while (!entrypossible);
+        // Entry possible now
+
+        buf->mutex[i].lock();
+        memcpy(buf->buf[i] + buf->offset[i], &chunk, sizeof(chunk));
+        buf->offset[i] += sizeof(chunk);
+        buf->mutex[i].unlock();
+    }
+
+
+    // Report
+    std::string ex = "FrameStreamHandler exits: ";
+    switch (exit_reason) {
+        case 0:
+            ex = ex + "Could not access stream " + streamloc;
+            break;
+        case 1:
+            ex = ex + "Syncbool set to false";
+            break;
+        case 2:
+            ex = ex + "Reached eof on file " + streamloc;
+            break;
+        default:
+            ex = ex + "Unknown";
+            break;
+    }
+    std::cout << ex << std::endl;
+    *syncbool = false;
 }
 
-void RawStreamHandler::FrameDiskHandler(bool *syncbool, FrameBuffer *buf, const char *dumploc){
+void RawStreamHandler::FrameDiskHandler(bool *syncbool, FrameBuffer *buf, const char *dumploc) {
     return;
 }
