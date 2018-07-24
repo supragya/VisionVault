60fcf0d139877be34282838989045f1a89abae10
Supragya Raj <supragyaraj@gmail.com>
2018-07-24 11:13:15 +0530

    Join done

--- src/stream_handler/StreamHandler.cpp
+++ src/stream_handler/StreamHandler.cpp
@@ -6,12 +6,15 @@ int main(){
     std::string low_speed_link = "../processed_data/cam_metadata.rawinfo";
     std::string high_speed_output = "../processed_data/RSH_frameCache.rawdata";
     std::string low_speed_output = "../processed_data/RSH_meteCache.rawinfo";
+    std::string final_mlv = "../processed_data/AXIOMBetaVideo.mlv";
 
     std::thread HSH (high_speed_handler, high_speed_link, high_speed_output);
     std::thread LSH (low_speed_handler, low_speed_link, low_speed_output);
 
     HSH.join();
     LSH.join();
+
+    system(("cat "+low_speed_output+" "+high_speed_link+" > "+final_mlv).c_str());
 
     return 0;
 }
